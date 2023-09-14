d93edb18df2aa7a5c332cc68fedca20810134f8b
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-14 19:15:35 +0500

    Create App.tsx

---
+++ galaxyproject/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./NavBar"; // Assuming the component file is named NavBar.tsx
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      {/* Other content of your application */}
+    </div>
+  );
+}
+
+export default App;
