705b9fa280321f77abec89456a5108b96c1cc9c7
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-15 19:11:01 +0500

    Update App.tsx

--- galaxyproject/src/App.tsx
+++ galaxyproject/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import NavBar from "./NavBar"; // Assuming the component file is named NavBar.tsx
+import About from "./about";
+import indexcss from "./index.css";
 
 function App() {
   return (
     <div>
       <NavBar />
       {/* Other content of your application */}
+      <About />
     </div>
   );
 }
