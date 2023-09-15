076a98ed4e18ab492e0efe3191442fa96fedc6e7
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-15 19:10:59 +0500

    Create About.tsx

---
+++ galaxyproject/src/About.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import indexcss from "/src/index.css";
+
+const About: React.FC = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <p>
+          Welcome to our website, your ultimate destination for<br></br> free
+          movies and songs! We are dedicated to providing<br></br> you with a
+          vast and diverse collection of<br></br> entertainment, all at your
+          fingertips, without any cost. Our mission is to bring joy and
+          excitement to your life<br></br> through the magic of cinema and
+          music, all while<br></br> ensuring a seamless and user-friendly
+          experience.
+        </p>
+      </div>
+    </div>
+  );
+};
