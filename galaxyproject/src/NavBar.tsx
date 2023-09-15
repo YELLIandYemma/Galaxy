a91d2c3efbdc7552e9cebbe6054fc1e2ee6c5b0d
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-15 19:46:24 +0500

    Update NavBar.tsx

--- galaxyproject/src/NavBar.tsx
+++ galaxyproject/src/NavBar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import indexcss from "/src/index.css";
-
+import About from "./About";
 const NavBar: React.FC = () => {
   return (
     <center>
       <nav>
         <ul>
           <li>
-            <a href="/">Home</a>
+            <a href="#about ">Home</a>
           </li>
           <li>
             <a href="/about">About</a>
