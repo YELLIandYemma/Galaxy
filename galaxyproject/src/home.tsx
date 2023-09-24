6fbe36a5a4a2827993ce9f76bbb10dcb6dcfc2d9
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-24 20:11:09 +0500

    Create home.tsx

---
+++ galaxyproject/src/home.tsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+const Home: react.FC = () => {
+  return (
+    <div className="container text-center">
+      <h1>Galaxy</h1>
+      <br></br>
+      <h1>
+        An Environment where you<br></br> can find all the movies and<br></br>{" "}
+        music for free.
+      </h1>
+    </div>
+  );
+};
+export default Home;
