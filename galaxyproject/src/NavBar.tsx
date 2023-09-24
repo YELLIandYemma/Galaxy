c753151fd6747c2bcd7c1a21cef417b97c0ea262
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-24 20:11:19 +0500

    Update NavBar.tsx

--- galaxyproject/src/NavBar.tsx
+++ galaxyproject/src/NavBar.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import indexcss from "/src/index.css";
+import { useState, useEffect } from "react";
 import About from "./About";
+import "./index.css";
+
 const NavBar: React.FC = () => {
   return (
-    <center>
-      <nav>
-        <ul>
-          <li>
-            <a href="#header-img">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="/invest">Invest</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </center>
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="/invest">Invest</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
