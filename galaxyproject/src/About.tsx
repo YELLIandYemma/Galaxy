7f86dc39142e446a5035ba7631911612ce4e0271
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-26 18:54:24 +0500

    Update About.tsx

--- galaxyproject/src/About.tsx
+++ galaxyproject/src/About.tsx
@@ -3,18 +3,22 @@ import "./index.css";
 const About: React.FC = () => {
   return (
     <div id="about">
-      <div className="about">
-        <h1>About</h1>
-        <p>
+      <div className="about" data-aos="fade-up">
+        <h1 data-aos="fade-up">About</h1>
+        <p data-aos="fade-up">
           Welcome to our website, your ultimate destination for<br></br> free
           movies and songs! We are dedicated to providing<br></br> you with a
           vast and diverse collection of<br></br> entertainment, all at your
-          fingertips, without any cost. Our mission is to bring joy and
+          fingertips, without any cost.<br></br> Our mission is to bring joy and
           excitement to your life<br></br> through the magic of cinema and
           music, all while<br></br> ensuring a seamless and user-friendly
           experience.
         </p>
       </div>
+      <img
+        src="https://static1.s123-cdn-static-a.com/uploads/2031/800_5a16bb1164caa.jpg"
+        alt="about"
+      ></img>
     </div>
   );
 };
