f65a52d060039ccc4c9ed1ee912a29b2e805b7ea
Yelli <133573446+YELLIandYemma@users.noreply.github.com>
2023-09-29 09:45:49 +0500

    Update NavBar.tsx

--- galaxyproject/src/NavBar.tsx
+++ galaxyproject/src/NavBar.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 const NavBar: React.FC = () => {
   return (
-    <nav className="navbar navbar-expand-lg text-end  bg-body-tertiary fixed-top ">
+    <nav className="navbar navbar-expand-lg text-end  bg-body-tertiary fixed-top bg-black ">
       <div className="container-fluid">
         <nav className="navbar bg-body-tertiary">
           <div className="container-fluid">
@@ -18,35 +18,39 @@ const NavBar: React.FC = () => {
         </nav>
 
         <button
-          className="navbar-toggler"
+          className="navbar-toggler text-black"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
           aria-controls="navbarNav"
-          aria-expanded="false"
+          aria-expanded="true"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon text-black"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse text-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <a
+                className="nav-link active text-black"
+                aria-current="page"
+                href="#"
+              >
                 Home
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#about">
+            <li className="nav-item text-black">
+              <a className="nav-link text-black" href="#about">
                 About
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#package">
+            <li className="nav-item text-black">
+              <a className="nav-link text-black" href="#package">
                 Packages
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#Contact">
+            <li className="nav-item text-black">
+              <a className="nav-link text-black" href="#Contact">
                 Contact
               </a>
             </li>
